8381ee7cc70811b256864a97cbcf3386bcb1b46a
Tanjuate Achaleke <tanjubrunostar0@gmail.com>
2025-08-11 10:47:45 +0100

    Made changes to the live endpoint

--- routes/live.js
+++ routes/live.js
@@ -2,7 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-// Get cart
+/**
+ * Keep alive
+ * GET /api/live
+ */
 router.get("/", async (req, res) => {
-  res.json({ "res": "Yay!! we are live!!!" });
+    try {
+        res.status(200).json({ "res": "Yay!! we are live!!!" });
+    } catch (error) {
+        console.error("Error ", error);
+        res.status(500).json({ error: "Failed to keep alive" });
+    }
 });
