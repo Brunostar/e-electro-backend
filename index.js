a2f35d0d8551b8561d69a13c8744ea3775d27989
Tanjuate Achaleke <tanjubrunostar0@gmail.com>
2025-08-11 11:16:46 +0100

    Added the route api/live in index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use("/api/products", require("./routes/products"));
 app.use("/api/cart", require("./routes/cart"));
 app.use("/api/orders", require("./routes/orders"));
 app.use("/api/reviews", require("./routes/reviews"));
+app.use("/api/live", require("./routes/live"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
